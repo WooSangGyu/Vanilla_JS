e9f3eb1cbd1751deae66ddc20da5fc8ebcbfb9cf
WooSangGyu <wsg0701@naver.com>
2020-09-10 14:53:55 +0900

    Datasort - Object

--- theory/Data_sort.js
+++ theory/Data_sort.js
@@ -1,3 +1,4 @@
+
 const monday = "Mon";
 const tue = "Tue";
 const wed = "Wed";
@@ -6,18 +7,49 @@ const fri = "Fri";
 
 console.log( monday, tue, wed, thu, fri);
 
+
 /*
-Camel case
+ Camel case
 - always a start with lowercase and if there is a space you start with uppercase on the next word.
 */
 
+/* This is Array
+
 const something = "Something"
 
-// Array
 const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", something, 5351, true];
 
-// console.log(daysOfWeek[2]);
-// output Wed
-// because Array first = 0
+console.log(daysOfWeek[2]); -> output Wed
+because Array first = 0
 
 console.log(daysOfWeek);
+*/
+
+// This is Object
+const Sanggyuinfo = {
+    name : "Sanggyu",
+    age : 26,
+    gender : "Male",
+    isHandsome : true,
+    favMovie : ["Along the gods", "LOTR", "Oldboy"],
+    favFood : [
+        { 
+            name:"Kimchi",
+            fatty:"false"
+        },
+        {
+            name:"Cheese burger",
+            fatty: true
+        }
+    ]
+}
+
+/*
+console.log(Sanggyuinfo.gender);
+
+Sanggyuinfo.gender = "Female";
+
+console.log(Sanggyuinfo.gender);
+*/
+
+console.log(Sanggyuinfo.favFood[0].name);
