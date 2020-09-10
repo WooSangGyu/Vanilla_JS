2e7a5e3d66a8867d0d3700ec58a1b492f7fd3121
WooSangGyu <wsg0701@naver.com>
2020-09-10 11:10:00 +0900

    Data Type JS

--- theory/index.js
+++ theory/index.js
@@ -4,21 +4,35 @@
 a = 221;
 b = a - 5;
 console.log(b);
-//output 216
+// output 216
 
-// Variable
-// first - Create
-// second - Initialize
-// third - Use
+/*
+Variable
+first - Create
+second - Initialize
+third - Use
+*/
 
 let A = 221;
 let B = A - 5;
 A = 4
 console.log(B, A); 
-//output 216 4
+// output 216 4
 
+/*
 const AA = 221;
 const BB = AA - 5;
 AA = 4;
 console.log(BB, AA);
-//output 
+- output Assignment to constant variable.
+- because const can't change variable value;
+*/
+var AAA = 221;
+var BBB = AAA - 5;
+AAA = 4;
+console.log(BBB, AAA);
+// output 216 4
+
+// String
+const what = "SangGyu"
+console.log(what);
